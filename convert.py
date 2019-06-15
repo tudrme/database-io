49b8984b615909ac198d273f2f1f1dc6fa1fc1d4
Gideon Tong <gideon@splimtech.com>
2019-06-14 23:23:43 -0700

    Got the name

--- convert.py
+++ convert.py
@@ -2,3 +2,25 @@
 
 # Imports
 import json
+import random
+import string
+
+# Home Directory
+home = "D:/Repos/database-io"
+
+# Generate a random ID to assign to tutors
+def generate(length = 20):
+    options = string.ascii_letters + string.digits
+    return ''.join(random.choice(options) for i in range(length))
+
+tutors = {}
+
+file = open(home + "/imports/tutors/araza_rahul.md", "r")
+
+contents = file.readlines()
+
+name = contents[0][2:len(contents) - 2]
+
+print(name)
+
+print(contents)
