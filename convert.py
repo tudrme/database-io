64c9b7d3a161c8d7f46fd98cd61b66fdbd61ed80
Gideon Tong <gideon@splimtech.com>
2019-06-14 23:32:24 -0700

    Got the teaching style

--- convert.py
+++ convert.py
@@ -13,21 +13,25 @@ def generate(length = 20):
     options = string.ascii_letters + string.digits
     return ''.join(random.choice(options) for i in range(length))
 
+# Create the database
 data = {}
 data['tutors'] = {}
 
 file = open(home + "/imports/tutors/araza_rahul.md", "r")
-
 contents = file.readlines()
 
 next_id = generate()
 data['tutors'][next_id] = {}
 
 name = contents[0][2:len(contents) - 2]
+data['tutors'][next_id]['name'] = name
+
+style_index = contents.index('## Teaching Style\n') + 2
+style = contents[style_index]
+data['tutors'][next_id]['bio'] = {}
+data['tutors'][next_id]['bio']['style'] = style
 
 
-print(next_id)
-
-print(contents)
+print(style)
 
 print(json.dumps(data, indent = 2))
