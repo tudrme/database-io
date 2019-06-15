09acc563c8005cc9d1b0725baf9cce448c276bd4
Gideon Tong <gideon@splimtech.com>
2019-06-14 23:28:04 -0700

    Random ID generator

--- convert.py
+++ convert.py
@@ -13,14 +13,21 @@ def generate(length = 20):
     options = string.ascii_letters + string.digits
     return ''.join(random.choice(options) for i in range(length))
 
-tutors = {}
+data = {}
+data['tutors'] = {}
 
 file = open(home + "/imports/tutors/araza_rahul.md", "r")
 
 contents = file.readlines()
 
+next_id = generate()
+data['tutors'][next_id] = {}
+
 name = contents[0][2:len(contents) - 2]
 
-print(name)
+
+print(next_id)
 
 print(contents)
+
+print(json.dumps(data, indent = 2))
