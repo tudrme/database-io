e7e0e880005a4e2df8d108fb481f95422de6a47d
Gideon Tong <gideon@splimtech.com>
2019-08-14 19:35:26 -0700

    Rewrote the image convert batch

--- image-convert.bat
+++ image-convert.bat
@@ -1,6 +1,7 @@
 REM Converts all images from one format into the .jpg file
-REM Intended for use from versions Tudrme v1 to Tudrme v2
 REM Requires ImageMagick 7 installed and in PATH env
+REM Is a simple enough script, simply iterates through
+REM all of the files provided
 
 for %%f in (imports/images/*) do (
        magick.exe convert "imports/images/%%~nf%%~xf" "exports/%%~nf.jpg"
